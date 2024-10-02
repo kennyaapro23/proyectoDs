7d7bc2cbce431935dd7910070cf5b883a8ff897d
kennyaapro23 <kennyaapro@hotmail.com>
2024-10-02 14:20:17 -0500

    sere

--- ms-catalog/src/main/java/com/example/mscatalog/MsCatalogApplication.java
+++ ms-catalog/src/main/java/com/example/mscatalog/MsCatalogApplication.java
@@ -1,7 +1,11 @@
 package com.example.mscatalog;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MsCatalogApplication {
@@ -9,5 +13,15 @@ public class MsCatalogApplication {
     public static void main(String[] args) {
         SpringApplication.run(MsCatalogApplication.class, args);
     }
-
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().info(new Info()
+            .title("OPEN API MICROSERVICIOS CATALOG")
+            .version("0.0.1")
+            .description("servicios web catálogo")
+            .termsOfService("http:// swagger.io/terms")
+            .license(new License().name("Apache 2.0").url("http://springdoc.org"))
+        );
+    }
 }
+
