0a155d2823014ade3f019e9c40ef85fa88bf51ca
kennyaapro23 <103528325+kennyaapro23@users.noreply.github.com>
2024-11-07 00:45:51 -0500

    Delete ms-user directory
    qwe

--- ms-user/src/main/java/com/example/msuser/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.msuser.repository;
-
-import com.example.msuser.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
