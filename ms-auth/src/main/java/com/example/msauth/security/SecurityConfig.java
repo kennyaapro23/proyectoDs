b06178d169fa66a119b05847fb2b8597bddef637
kennyaapro23 <kennyaapro@hotmail.com>
2024-11-07 01:09:01 -0500

    tokens

--- ms-auth/src/main/java/com/example/msauth/security/SecurityConfig.java
+++ ms-auth/src/main/java/com/example/msauth/security/SecurityConfig.java
@@ -1,12 +1,28 @@
 package com.example.msauth.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().anyRequest().permitAll();
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()  // Deshabilita CSRF (usa solo si es necesario)
+                .authorizeRequests()
+                .anyRequest().permitAll();  // Permite todas las solicitudes sin autenticación
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();  // Definir el encriptador de contraseñas
     }
 }
